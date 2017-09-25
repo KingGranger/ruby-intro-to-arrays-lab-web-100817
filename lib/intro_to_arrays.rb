d3102d195b128bf2cb82612a0c93b1aa896ded81
Brandon  Harris <cheeridon@gmail.com>
2017-09-25 19:07:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -23,3 +23,11 @@ end
 def first_element_with_array_methods(array)
   array.shift
 end
+
+def last_element_with_array_methods(array)
+  array.pop
+end
+
+def length_of_array(array)
+  array.length
+end
